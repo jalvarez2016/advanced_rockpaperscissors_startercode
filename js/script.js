453fbffd835f30a25d11be5e9af79d223ad78704
Jason <alvarezcoraiza@citypolyhs.org>
2017-11-30 03:24:58 +0000

    need a dog option but otherwise it works

--- js/script.js
+++ js/script.js
@@ -21,23 +21,69 @@
 //GLOBAL VARIABLES
  var you = "";
  var com = "";
- var winner = ""
+ var winner = "";
  var choises = ["rock","paper","sissors"];
   
 //FUNCTIONS
  
 function check() {
-        
+  if($("#type").val()=== "rock"){
+          return true;
+  } else if($("#type").val()=== "paper"){
+          return true;
+  } else if($("#type").val()=== "sissor"){
+          return true;
+  } else {
+          return false;
+  }
+}
+
+function computerChoise() {
+ var decider = Math.ceil(Math.random()*3);
+ console.log(decider);
+ if( decider === 1){
+        return com = "paper";
+ } else if ( decider === 2){
+        return com = "rock";
+ } else if ( decider === 3){
+        return com = "sissor";
+ }
+}
+
+function decideWinner(){
+ if (you === "rock" && com === "paper"){
+        $("#fate").html("congrates you get to die");
+ } else if (you === "rock" && com === "rock"){
+        $("#fate").html("welp . . . try again");
+ } else if (you === "rock" && com === "sissor"){
+        $("#fate").html(" congrates you get to live");
+ } else if (you === "paper" && com === "rock"){
+        $("#fate").html(" congrates you get to live another day");
+ } else if (you === "paper" && com === "paper"){
+        $("#fate").html("welp . . . try again");
+ } else if (you === "paper" && com === "sissor"){
+        $("#fate").html("com congrates you get to die");
+ } else if (you === "sissor" && com === "rock"){
+        $("#fate").html(" congrates you get to die");
+ } else if (you === "sissor" && com === "paper"){
+        $("#fate").html(" congrates you get to live");
+ } else if (you === "sissor" && com === "sissor"){
+        $("#fate").html("welp . . . try again");
+ }
 }
 
 // DOCUMENT READY FUNCTION
 /*global $*/
     $(document).ready(function() {
         console.log("human");
+        console.log(computerChoise());
         $("#submit").click(function(){
-           if(check){
+           if(check()){
                 you = $("#type").val();
                 console.log(you);
+                $("#you").html("you : " + ($("#type").val()));
+                $("#com").html("Computers pick : " + computerChoise());
+                decideWinner();
            } else {
                 console.log("invalid");
            }
